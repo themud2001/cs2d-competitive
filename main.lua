a014b290cfc23d0af84798ac71e274c93d15f7a3
THEMUD <themud2001@hotmail.com>
2020-09-09 13:04:17 +0200

    Added the functions.lua file to the dofile list

--- main.lua
+++ main.lua
@@ -2,4 +2,5 @@ local scriptPath = debug.getinfo(1, "S").short_src:match("(.*/)");
 
 dofile(scriptPath.."core/config.lua");
 dofile(scriptPath.."core/Player.lua");
+dofile(scriptPath.."core/functions.lua");
 dofile(scriptPath.."core/core.lua");
