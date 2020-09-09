91e6e4341dbe602c3e9ccdb4c379bf67c619a146
THEMUD <themud2001@hotmail.com>
2020-09-09 13:03:30 +0200

    Removed the excessive indentation

--- core/functions.lua
+++ core/functions.lua
@@ -1,8 +1,6 @@
 function getRoundMVP(dmgTable)
 	local mvp = math.max(unpack(dmgTable));
 	for id, dmg in pairs(dmgTable) do
-		if(dmg == mvp) then
-			return id;
-		end
+		if(dmg == mvp) then return id; end
 	end
 end
