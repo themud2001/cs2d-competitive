3b61df81010a89b4c5b6643c80013f531c43c6ee
THEMUD <themud2001@hotmail.com>
2020-09-12 16:16:29 +0200

    Edited the ranks points

--- core/config.lua
+++ core/config.lua
@@ -40,48 +40,48 @@ _ranks = {
 	[1] = {
 		name = "Bronze";
 		tag = "\169237098005[Bronze]";
-		points = 10;
+		points = 75;
 	};
 
 	[2] = {
 		name = "Silver";
 		tag = "\169130130130[Silver]";
-		points = 50;
+		points = 175;
 	};
 
 	[3] = {
 		name = "Gold";
 		tag = "\169240210000[Gold]";
-		points = 75;
+		points = 300;
 	};
 
 	[4] = {
 		name = "Emerald";
 		tag = "\169033232002[Emerald]";
-		points = 1001;
+		points = 450;
 	};
 
 	[5] = {
 		name = "Diamond";
 		tag = "\169000210252[Diamond]";
-		points = 1000;
+		points = 625;
 	};
 
 	[6] = {
 		name = "Guardian";
 		tag = "\169163000205[Guardian]";
-		points = 1000;
+		points = 800;
 	};
 
 	[7] = {
 		name = "Supreme";
 		tag = "\169000150255[S\169000125255u\169000100255p\169000080255r\169000065255e\169000035255m\169000000255e]";
-		points = 1000;
+		points = 975;
 	};
 
 	[8] = {
 		name = "Overlord";
 		tag = "\169000000000[Overlord]";
-		points = 1000;
+		points = 1150;
 	};
 };
